136f507648329e73dc9049ea46b596cd4e6f09c9
wannacry3030 <wannacry.bat@proton.me>
2022-12-13 20:13:23 -0300

    finished

--- apirest.py
+++ apirest.py
@@ -1,4 +1,23 @@
 import pandas as pd
+from flask import Flask, jsonify
 
-tabela =  pd.read_csv('advertise.csv')
-print(tabela)
+app = Flask(__name__)
+
+#construindo as funcionalidades
+@app.route('/')
+def homepage():
+  return 'Essa é a home'
+
+@app.route('/pegarvendas')
+def pegarvendas():
+  tabela =  pd.read_csv('advertise.csv')
+  total_vendas = tabela['Vendas'].sum()
+  resposta = {'total_vendas': total_vendas}
+  return jsonify(resposta)
+
+#rodando a api
+app.run()
+
+# tabela =  pd.read_csv('advertise.csv')
+# total_vendas = tabela['Vendas'].sum()
+# print(total_vendas)
